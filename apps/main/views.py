1d4d97ed259d567ca968908e3414de8d549f002a
Ronny Abraham <this.ronny@gmail.com>
2010-04-13 19:38:44 +0300

    modified:   apps/main/views.py 	modified:   media/css/administrative.css 	modified:   media/css/base.css 	modified:   media/css/bio.css 	modified:   media/css/main.css 	deleted:    media/css/sifr.css 	new file:   media/font/manksans/manksans.eot 	new file:   media/font/manksans/manksans.ttf 	new file:   media/font/manksans/manksans.woff 	new file:   media/font/manksans/manksansmedium.eot 	new file:   media/font/manksans/manksansmedium.ttf 	new file:   media/font/manksans/manksansmedium.woff 	new file:   media/font/manksans/manksansmediumoblique.eot 	new file:   media/font/manksans/manksansmediumoblique.ttf 	new file:   media/font/manksans/manksansmediumoblique.woff 	new file:   media/font/manksans/manksansoblique.eot 	new file:   media/font/manksans/manksansoblique.ttf 	new file:   media/font/manksans/manksansoblique.woff 	deleted:    media/js/sifr-config.js 	deleted:    media/js/sifr-debug.js 	deleted:    media/js/sifr.js 	modified:   templates/department/dept.html 	modified:   templates/department/podiatry.html 	modified:   templates/main/base.html 	modified:   templates/main/menu.html 	modified:   templates/main/menu.xml 	modified:   templates/staff/medical.html 	modified:   templates/staff/medical/bio.html

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import Context, loader
 
-from parkhealth.settings import MEDIA_URL, BASE_URL
+from parkhealth.settings import MEDIA_URL
 from main.models import MedicalStaff, Specialty
 
 def index(request):
     return render_to_response('main/home.html', 
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+        { 'MEDIA_URL':MEDIA_URL, 
             'MENU':menu(section="home"), })
 
 
@@ -28,7 +28,7 @@ def department(request, page):
     medical_staff = MedicalStaff.objects.filter(specialty=specialty).order_by("name_last")
 
     return render_to_response(html_page, 
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+        { 'MEDIA_URL':MEDIA_URL, 
             'specialty':specialty, 'medical_staff':medical_staff,
             'MENU':menu(section="dept", subsection=page), })
 
@@ -38,7 +38,7 @@ def staff(request, section):
     medical_staff = MedicalStaff.objects.filter(staff_listing=True).order_by('name_last')
 
     return render_to_response(html_page, 
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+        { 'MEDIA_URL':MEDIA_URL,
             'medical_staff':medical_staff,
             'MENU':menu(section), })
 
@@ -49,48 +49,48 @@ def staff_bio(request, section, first, last):
     html_page = "staff/%s/%s.html" % (section, bio.handle())
 
     return render_to_response(html_page, 
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+            { 'MEDIA_URL':MEDIA_URL, 'bio':bio, 
             'MENU':menu(section, subsection, bio), })
 
 def community(request, section):
     html_page = "community/%s.html" % (section)
 
     return render_to_response(html_page,
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL,
+        { 'MEDIA_URL':MEDIA_URL,
             'MENU':menu(section="community", subsection="news"), })
                 
 def directions(request):
     html_page = "main/directions.html"
 
     return render_to_response(html_page,
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL,
+        { 'MEDIA_URL':MEDIA_URL,
             'MENU':menu(section="directions"), })
 
 def insurance(request):
     html_page = "main/insurance.html"
 
     return render_to_response(html_page,
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL,
+        { 'MEDIA_URL':MEDIA_URL,
             'MENU':menu(section="insurance"), })
 
 def contact(request):
     html_page = "main/contact.html"
 
     return render_to_response(html_page,
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL,
+        { 'MEDIA_URL':MEDIA_URL,
             'MENU':menu(section="contact"), })
 
 def about(request):
     html_page = "main/about.html"
 
     return render_to_response(html_page,
-        { 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+        { 'MEDIA_URL':MEDIA_URL,
             'MENU':menu(section="about"), })
 
 def menu(section="", subsection="", bio=""):
     specialties = Specialty.objects.filter(is_menu=True)
     t = loader.get_template('main/menu.html')
-    c = Context({ 'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 'specialties':specialties, 
+    c = Context({ 'MEDIA_URL':MEDIA_URL, 'specialties':specialties, 
         'section':section, 'subsection':subsection, 'bio':bio})
                         
     return t.render(c)
@@ -99,7 +99,7 @@ def menu(section="", subsection="", bio=""):
 #    specialties = Specialty.objects.filter(is_menu=True)
 #    
 #    return render_to_response('main/menu.xml', 
-#                {   'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+#                {   'MEDIA_URL':MEDIA_URL,
 #                    'specialties':specialties, 
 #                    'section':section, 'subsection':subsection})
 
@@ -111,6 +111,6 @@ def menu(section="", subsection="", bio=""):
 #
 #    specialties = Specialty.objects.filter(is_menu=True)
 #    return render_to_response('main/menu.xml', 
-#                    {   'MEDIA_URL':MEDIA_URL, 'BASE_URL':BASE_URL, 
+#                    {   'MEDIA_URL':MEDIA_URL,
 #                        'bio':bio, 'specialties':specialties,
 #                        'section':section, 'subsection':subsection})
