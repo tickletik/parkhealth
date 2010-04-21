0470cea7f1e6a7248b525675080cc37a4867cbff
Ronny Abraham <this.ronny@gmail.com>
2010-04-21 20:01:06 +0300

    modified:   development.sh

--- scripts/path/webfaction/development.sh
+++ scripts/path/webfaction/development.sh
@@ -4,5 +4,5 @@ export PATH
 DJANGO_SETTINGS_MODULE="parkhealth.settings"
 export DJANGO_SETTINGS_MODULE
 
-PYTHONPATH=$PYTHONPATH:"/home/parkadmin/webapps/django_development/lib/apps"
+PYTHONPATH=$PYTHONPATH:"/home/parkadmin/webapps/django_development/lib/apps":"/home/parkadmin/webapps/django_development/lib/python2.5"
 export PYTHONPATH
