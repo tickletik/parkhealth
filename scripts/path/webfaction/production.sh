c97854ab98cda791a62fca65ad971cff6ace9657
parkadmin <parkadmin@web124.webfaction.com>
2010-04-21 16:15:56 -0500

    modified:   production.sh

--- scripts/path/webfaction/production.sh
+++ scripts/path/webfaction/production.sh
@@ -4,5 +4,6 @@ export PATH
 DJANGO_SETTINGS_MODULE="parkhealth.settings"
 export DJANGO_SETTINGS_MODULE
 
-PYTHONPATH=$PYTHONPATH:"/home/parkadmin/webapps/django_production/lib/apps"
+PYTHONPATH=$PYTHONPATH:"/home/parkadmin/webapps/django_production/lib/apps":"/home/parkadmin/webapps/django_production/lib/python2.5"
+
 export PYTHONPATH
